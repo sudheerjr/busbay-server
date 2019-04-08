c5946196b91dfa5db305b4840e63f8f58939af18
Prabha Shankar <shankar75031@gmail.com>
2019-04-09 01:52:31 +0530

    added place and distance function that adds location name and distance to college when coordinates are entered

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require("firebase-functions");
-const axios = require('axios')
+const axios = require("axios");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -13,7 +13,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.reformatReadings = functions.database
-  .ref("/BUS_TRACKING/{cardNo}/{pushId}")
+  .ref("/BUS_TRACKING_EVENING/{cardNo}/{pushId}")
   .onCreate((snapshot, context) => {
     const reading = snapshot.val();
     var splitReading = reading.split("#");
@@ -30,17 +30,17 @@ exports.reformatReadings = functions.database
     };
     var reformattedDate = date.replace("/", "_");
     reformattedDate = reformattedDate.replace("/", "_");
-    admin
+    return admin
       .database()
       .ref(`/rfid_uid_mapping/`)
       .child(rfid)
       .once("value", snapshot => {
         const userId = snapshot.val();
-        admin
-          .database()
-          .ref(`bus_morning/${userId}`)
-          .child(`date${reformattedDate}`)
-          .set(reformattedReading);
+        // admin
+        //   .database()
+        //   .ref(`bus_morning/${userId}`)
+        //   .child(`date${reformattedDate}`)
+        //   .set(reformattedReading);
 
         var googleDirectionsUrl =
           "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=" +
@@ -53,25 +53,26 @@ exports.reformatReadings = functions.database
           76.329194 +
           "&key=AIzaSyA-AuKqE-g9wAfwN9g99-BcWy_-oAN_V0c";
 
+        var googlePlacesUrl =
+          "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+          lat +
+          "," +
+          lng +
+          "&key=AIzaSyA-AuKqE-g9wAfwN9g99-BcWy_-oAN_V0c";
         axios
-          .get(googleDirectionsUrl)
-          .then(function(response) {
-              console.log("fetched");
-              
-            return admin
-              .database()
-              .ref("test/")
-              .set(
-                {
-                  response: response.data.rows[0].elements[0].distance.text
-                },
-                function(error) {
-                  if (error) {
-                     console.error(error);
-                  }
-                }
-              );
-          })
+          .all([axios.post(googlePlacesUrl), axios.get(googleDirectionsUrl)])
+          .then(
+            axios.spread((placesResponse, directionsResponse )=> {
+              const distance = directionsResponse.data.rows[0].elements[0].distance.text;
+              const fromLocation = placesResponse.data.results[0].address_components[1].short_name;
+              const locationDetails = { distance, fromLocation};
+              return admin
+                .database()
+                .ref(`guio/`)
+                .child(`locationDetails`)
+                .set(locationDetails);
+            })
+          )
           .catch(function(error) {
             return console.log(error);
           });
